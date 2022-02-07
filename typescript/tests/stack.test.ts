797784575c77f28924991d62465f92d59a9c5a60
David Taing <adavidtaing@gmail.com>
2022-02-07 20:51:19 +1100

    test: Stack empty, push & pop functions

--- typescript/tests/stack.test.ts
+++ typescript/tests/stack.test.ts
@@ -1,13 +1,45 @@
 import Stack from "../stack";
 
 describe("Stack<String>", () => {
-  let stack: Stack<string>;
-
-  beforeAll(() => {
+  describe("Stack.empty with empty stack", () => {
+    let stack: Stack<string>;
     stack = new Stack<string>();
+
+    test("Equals true", () => {
+      expect(stack.empty()).toEqual(true);
+    });
   });
 
-  test("Stack.empty", () => {
-    expect(stack.empty()).toEqual(true);
+  describe("Stack.push adds Hello to Stack", () => {
+    let stack: Stack<string>;
+    stack = new Stack<string>();
+    stack.push("Hello");
+
+    test("stack.top is now 1", () => {
+      expect(stack.top === 1);
+    });
+
+    test("'Hello' is in stack", () => {
+      expect(stack.items[0] === "Hello");
+    });
+  });
+
+  describe("Stack.pop with empty stack", () => {
+    let stack: Stack<string>;
+    stack = new Stack<string>();
+
+    test("throws 'Stack Underflow' error", () => {
+      expect(() => stack.pop()).toThrowError("Stack Underflow");
+    });
+  });
+
+  describe("Stack.pop with empty stack", () => {
+    let stack: Stack<string>;
+    stack = new Stack<string>();
+    stack.push("Hello");
+
+    test("returns 'Hello'", () => {
+      expect(stack.pop()).toStrictEqual("Hello");
+    });
   });
 });
