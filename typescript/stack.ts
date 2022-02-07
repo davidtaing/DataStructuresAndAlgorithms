c10e01c705e8756d4912fe2762b822b18d6faba8
David Taing <adavidtaing@gmail.com>
2022-02-07 20:28:44 +1100

    implement stack in typescript

---
+++ typescript/stack.ts
@@ -0,0 +1,24 @@
+class Stack<T> {
+  top = 0;
+  items: Array<T> = new Array<T>();
+
+  empty() {
+    if (this.top === 0) return true;
+
+    return false;
+  }
+
+  push(item: T) {
+    this.top += 1;
+    this.items[this.top] = item;
+  }
+
+  pop() {
+    if (this.empty()) {
+      throw Error("Stack Underflow");
+    } else {
+      this.top -= 1;
+      return this.items[this.top + 1];
+    }
+  }
+}
